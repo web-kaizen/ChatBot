0d0522f73a5238202ca2e73de345c594f6c30078
MaksymPastukh <saponexamq@gmail.com>
2023-12-20 19:51:57 +0100

    main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import TheMainContent from "./components/MainContent/TheMainContent.vue";
 
 import './assets/styles/style.css'
 
-const app = createApp(App);
-
-app.component('main-component', TheMainContent);
-app.mount('#app')
-
+createApp(App).mount('#app')
