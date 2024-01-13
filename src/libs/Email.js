fe4047d521273bcb39da1a5c8cbd563db05a494d
evgcursed <evgcursed@gmail.com>
2024-01-13 16:19:49 +0500

    src/libs Email.js: добавил метод check и проверку параметров

--- src/libs/Email.js
+++ src/libs/Email.js
@@ -1,5 +1,8 @@
 import ApiError from './ApiError.js';
 
 export default class Email {
-
+    check(token, callback) {
+        if (typeof token !== 'string') ApiError.return('invalid_token');
+        if (typeof callback !== 'function') ApiError.return('invalid_callback');
+    }
 }
