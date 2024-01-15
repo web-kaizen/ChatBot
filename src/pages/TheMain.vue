6809957647ac22f3219947982edcb9d91e5a036a
PSKutergin <PSKutergin@gmail.com>
2024-01-15 10:04:00 +0300

    устранил конфликты

--- src/pages/TheMain.vue
+++ src/pages/TheMain.vue
@@ -1,7 +1,34 @@
 <script setup>
+import TheSidebar from "../components/Sidebar/TheSidebar.vue";
 import TheMainContent from "@/components/MainContent/TheMainContent.vue";
+import TheModal from "../components/Modal/TheModal.vue";
+
+import { ref } from "vue";
+
+const isOpenModal = ref(false);
+const user = ref("");
+
+const getUser = (email) => {
+  user.value = email;
+};
+
+const updUser = (value) => {
+  user.value = value;
+};
 </script>
 
 <template>
-  <TheMainContent />
+  <section class="layout">
+    <TheSidebar
+      @toggle-modal="isOpenModal = !isOpenModal"
+      @update-props="updUser"
+      :user="user"
+    />
+    <TheModal
+      :isOpen="isOpenModal"
+      @toggle-modal="isOpenModal = !isOpenModal"
+      @send-email="getUser"
+    />
+    <TheMainContent />
+  </section>
 </template>
