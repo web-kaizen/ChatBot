f4968331b855a58636b1c925b574af8cb54a1d37
evgcursed <evgcursed@gmail.com>
2024-01-15 14:33:21 +0500

    src/libs Emails.js: сделал метод resend

--- src/libs/Email.js
+++ src/libs/Email.js
@@ -5,7 +5,7 @@ export default class Email {
         if (typeof token !== 'string') ApiError.return('invalid_token');
         if (typeof callback !== 'function') ApiError.return('invalid_callback');
 
-        let url = 'http://85.215.65.210:8081/users/email-verification/check'
+        let url = 'http://85.215.65.210:8081/api/v0/users/email-verification/check'
 
         fetch(url, {
             method: 'GET',
@@ -31,19 +31,62 @@ export default class Email {
             .catch(error => console.log(error));
     }
     resend(token, callback) {
+        if (typeof token !== 'string') ApiError.return('invalid_token');
+        if (typeof callback !== 'function') ApiError.return('invalid_callback');
+
+        let url = 'http://85.215.65.210:8081/api/v0/users/email-verification/resend'
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+        })
+            .then(async function(response) {
+                let status = response.status
+                if (status === 204) {
+                    return {
+                        "status": true,
+                        "ttl": 120
+                    }
+                } else if (status === 401) {
+                    throw new Error('Ошибка авторизации (401)')
+                } else if (status === 429) {
+                    let json = await response.json();
+                    return {
+                        "status": true,
+                        "ttl": json['error']['delay']
+                    }
+                } else if (status === 500) {
+                    throw new Error('Server error (500)')
+                } else {
+                    console.log('Без статуса хз..')
+                }
+            })
+            .then(data => callback(data))
 
     }
     verify(token, email, code, callback) {
-
+        if (typeof token !== 'string') ApiError.return('invalid_token');
+        if (typeof email !== 'string') ApiError.return('invalid_email');
+        if (typeof code !== 'number') ApiError.return('invalid_code');
+        if (typeof callback !== 'function') ApiError.return('invalid_callback');
     }
 }
 const myCallback = (value) => {
-    console.log("Значение:", value);
+    console.log(value)
 };
 
 let email = new Email()
 let token = '869539d6ca02ae7a90593fa2392e2422dfd2546052b7a6b7ffa3b85f185a6e9d'
+let email2 = 'evgcursed@gmail.com'
+let code = 12345
 
-email.check(token, myCallback)
-email.resend()
-email.verify()
+// console.log('Method: check (working)')
+// email.check(token, myCallback)
+
+console.log('Method: resend (проблема: много отправил запросов и сервер перестал отвечать => (не затестировал метод)')
+email.resend(token, myCallback)
+
+// console.log('Method: verify')
+// email.verify(token, email2, code, myCallback)
