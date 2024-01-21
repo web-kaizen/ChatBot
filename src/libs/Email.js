67c82132b742d45dc675d27ccf09b625dc30c0d6
evgcursed <evgcursed@gmail.com>
2024-01-22 01:20:30 +0500

    src/libs Email.js: в запросе исправил статусы на код

--- src/libs/Email.js
+++ src/libs/Email.js
@@ -15,23 +15,11 @@ export default class Email {
             },
         })
             .then(async function(response) {
-                let status = response.status;
-                if (status === 200) {
-                    let json = await response.json();
-                    return {
-                        "status": json['result']['status']
-                    };
-                } else if (status === 401) {
-                    ApiError.return('invalid_access_token');
-                } else if (status === 404) {
-                    let json = await response.json();
-                    if (json['error']['code'] === 'version_not_found') {
-                        ApiError.return('version_not_found');
-                    } else if (json['error']['code'] === 'application_not_found') {
-                        ApiError.return('application_not_found');
-                    }
-                } else if (status === 500) {
-                    ApiError.return('server_error');
+                let json = await response.json();
+                if ('result' in json) {
+                    return { "status": json['result']['status'] }
+                } else if ('error' in json) {
+                    ApiError.return(json['error']['code'])
                 }
             })
             .then(data => callback(data));
@@ -50,22 +38,14 @@ export default class Email {
             },
         })
             .then(async function(response) {
-                let status = response.status;
-                if (status === 204) {
-                    return {
-                        "status": true,
-                        "ttl": 120
-                    };
-                } else if (status === 401) {
-                    ApiError.return('invalid_access_token');
-                } else if (status === 429) {
-                    let json = await response.json();
-                    return {
-                        "status": false,
-                        "ttl": json['error']['delay']
-                    };
-                } else if (status === 500) {
-                    ApiError.return('server_error');
+                if (response.status === 204) {
+                    return { "status": true, "ttl": 120 };
+                }
+                let json = await response.json();
+                if (response.status === 429) {
+                    return { "status": false, "ttl": json['error']['delay'] };
+                } else if ('error' in json) {
+                    ApiError.return(json['error']['code'])
                 }
             })
             .then(data => callback(data));
@@ -91,41 +71,14 @@ export default class Email {
             })
         })
             .then(async function(response) {
-                let status = response.status;
-                if (status === 204) {
-                    return {
-                        "status": true
-                    };
-                } else if (status === 400) {
-                    let json = await response.json();
-                    if (json['error']['code'] === 'invalid_code') {
-                        ApiError.return('invalid_code');
-                    } else if (json['error']['code'] === 'invalid_email') {
-                        ApiError.return('invalid_email');
-                    }
-                } else if (status === 401) {
-                    ApiError.return('invalid_access_token');
-                } else if (status === 403) {
-                    ApiError.return('email_access_denied');
-                } else if (status === 404) {
-                    let json = await response.json();
-                    if (json['error']['code'] === 'version_not_found') {
-                        ApiError.return('version_not_found');
-                    } else if (json['error']['code'] === 'application_not_found') {
-                        ApiError.return('application_not_found');
-                    }
-                } else if (status === 500) {
-                    ApiError.return('server_error');
+                if (response.status === 204) {
+                    return { "status": true };
+                }
+                let json = await response.json();
+                if ('error' in json) {
+                    ApiError.return(json['error']['code'])
                 }
             })
             .then(data => callback(data));
     }
 }
-const callback = (obj) => {
-    console.log(obj);
-};
-let email = new Email()
-let token = 'a42142af1fa222c4201a0a442ffa215667586e060243a6c5e924750bab2dd78c'
-
-email.check(token, callback)
-// email.resend(token, callback)
