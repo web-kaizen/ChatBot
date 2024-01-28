873c31edddea626d3799a2b6804f12f9a57d0690
evgcursed <evgcursed@gmail.com>
2024-01-28 17:07:53 +0500

    src/libs Dialogues.js исправил возврат

--- src/libs/Dialogues.js
+++ src/libs/Dialogues.js
@@ -19,11 +19,11 @@ export default class Dialogues {
         })
             .then(async function(response) {
                 if (response.status === 204) {
-                    return {'result': false}
+                    return null
                 }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -46,12 +46,9 @@ export default class Dialogues {
             },
         })
             .then(async function(response) {
-                if (response.status === 204) {
-                    return {'result': false}
-                }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -73,12 +70,9 @@ export default class Dialogues {
             },
         })
             .then(async function(response) {
-                if (response.status === 204) {
-                    return {'result': false}
-                }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -103,11 +97,11 @@ export default class Dialogues {
         })
             .then(async function(response) {
                 if (response.status === 204) {
-                    return {'result': false}
+                    return null
                 }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -132,11 +126,11 @@ export default class Dialogues {
         })
             .then(async function(response) {
                 if (response.status === 204) {
-                    return {'result': false}
+                    return null
                 }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -159,11 +153,11 @@ export default class Dialogues {
         })
             .then(async function(response) {
                 if (response.status === 204) {
-                    return { 'result': true }
+                    return null
                 }
                 let json = await response.json();
                 if ('result' in json) {
-                    return json
+                    return json['result']
                 } else if ('error' in json) {
                     ApiError.return(json['error']['code'])
                 }
@@ -171,15 +165,3 @@ export default class Dialogues {
             .then(data => callback(data));
     }
 }
-
-function callback(result) {
-    console.log(result); // Вывод результата в консоль
-}
-let token = '27b71028293f56d0f851cab13264809691dcddd1fd3439e626565f1f40f0df1f'
-
-let dialogues = new Dialogues()
-// dialogues.get(0, 0, callback) //работает
-// dialogues.make('gpt-4-8k', 1, callback) //invalid name
-// dialogues.getById(30, callback) //работает
-// dialogues.optUpdateById(30, 'bebra', 1, callback) //не понял как проверить но вроде работает...
-// dialogues.updateById(30, callback) // должен работать
