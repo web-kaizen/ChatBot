825a6fe1144084a07be679cdbaa533daafa7567e
PSKutergin <PSKutergin@gmail.com>
2024-02-04 17:21:16 +0300

    добавил верстку компонента со списком диалогов и компонента диалога

--- src/components/Sidebar/TheSidebar.vue
+++ src/components/Sidebar/TheSidebar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { defineProps, defineEmits } from 'vue'
 import TheSidebarActions from './TheSidebarActions.vue'
-import TheSidebarNavVue from './TheSidebarNav.vue'
+import TheSidebarNav from './TheSidebarNav.vue'
 import TheSidebarUser from './TheSidebarUser.vue'
 
 const { user } = defineProps(['user'])
@@ -12,7 +12,10 @@ const openModal = () => emit('toggle-modal')
 <template>
   <aside class="sidebar">
     <TheSidebarActions />
-    <TheSidebarNavVue />
+    <div class="sidebar__nav-wrapper">
+      <TheSidebarNav title="Сегодня" />
+      <TheSidebarNav title="Вчера" />
+    </div>
     <TheSidebarUser
       @toggle-modal="openModal"
       @update-props="emit('update-props', '')"
@@ -20,3 +23,14 @@ const openModal = () => emit('toggle-modal')
     />
   </aside>
 </template>
+
+<style scoped>
+.sidebar__nav-wrapper {
+  flex: 1;
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
--- src/components/Sidebar/TheSidebarNav.vue
+++ src/components/Sidebar/TheSidebarNav.vue
@@ -1,3 +1,33 @@
+<script setup>
+import SidebarNavItem from './SidebarNavItem.vue'
+
+defineProps({
+  title: String
+})
+</script>
+
 <template>
-  <nav class="sidebar-navigation"></nav>
+  <p class="sidebar-title">{{ title }}</p>
+  <nav class="sidebar-navigation">
+    <SidebarNavItem title="Диалог 1" />
+    <SidebarNavItem title="Диалог 2" />
+    <SidebarNavItem title="Диалог 3" />
+    <SidebarNavItem title="Диалог 4" />
+    <SidebarNavItem title="Диалог 5" />
+  </nav>
 </template>
+
+<style scoped>
+.sidebar-title {
+  padding-left: 10px;
+  color: #888;
+  font-size: 14px;
+}
+.sidebar-navigation {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: flex-start;
+}
+</style>
